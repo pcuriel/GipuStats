516150046157684b39216a6fa64a29034718f6ca
pcuriel <pcalinkin@gmail.com>
2015-02-26 19:39:03 +0100

    Working version of download.py. - Download fix: it was downloding the help pdf. Now it downloads only the two CSV files we need. - Any processing removed, now it only downloads and saves the data to the 'data/raw' folder.

--- DownloadAndCleanData/download.py
+++ DownloadAndCleanData/download.py
@@ -8,26 +8,24 @@ import pandas.io.parsers
 response = urllib2.urlopen('http://api.gipuzkoairekia.eus/dataset/buscar?texto=catastro&numRes=100000000')
 html = response.read()
 
-
 tree = ET.fromstring(html)
-files = []
+i = 0
 for dataset in tree.findall("datasets/dataset"):
-    titulo = dataset.find("titulo").text
-    titulo = titulo[titulo.index('(')+1:-1].title()
-    for recurso in dataset.findall("documentosAsociados/recurso/url"):
-        csv_text = urllib2.urlopen(recurso.text)
-        csv_text.skip
-        dataFrame = pandas.io.parsers.read_fwf(
-            csv_text,
-            [(0, 2), (4, 11), (13, 19), (21, 21), (23, 25), (27, 29), (31, 34), (36, 65), (67, 70),
-             (72, 73), (75, 76), (78, 80), (82, 83), (85, 93), (95, 96)])
-        files.append((titulo, csv_text, dataFrame))
+    title = dataset.find("titulo").text
+    title = title[title.index('(')+1:-1].title()
+    for resource in dataset.findall("recursos/recurso"):
+        tipo = ""
+        if resource.find("nombreFichero").text.startswith("ficherourbanaparcelamunicipio"):
+            tipo = "Parcelas"
+        else:
+            tipo = "Locales"
+        
+        resource_url = resource.find("url").text
+        print "Downloading", resource_url
+        f = urllib2.urlopen(resource_url)
+        csv = open("data/raw/" + title.replace("/", "-") + "_" + tipo + ".csv", "wb")
+        csv.write(f.read())
+        csv.close()
+        i += 1
 
-print dataFrame[1]
-for f in files:
-
-dataFrame()
-
-print "recursos encontrados: " + str(len(files))
-
-    #csv_text = urllib2.urlopen(a.text).read()
+print "Downloaded", i, "files"
